722b4da9d6154afba8775f40077753d2c95ab708
Guligena Aierken <guligena_aierken@163.com>
2019-10-23 19:52:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,8 +17,7 @@ def nyc_pigeon_organizer(data)
 def nyc_pigeon_organizer(data)
       new_hash = {}
       data.each do |key, value|
-        value.each do |color,name|
+      value.each do |color,name|
 
-
-        end
-      end
+    end     
+end
